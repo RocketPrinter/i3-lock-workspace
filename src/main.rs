85cc111c4d269983a98bfe4c3be982fc5a55b0f2
RocketPrinter <35203190+RocketPrinter@users.noreply.github.com>
2023-10-01 23:29:13 +0300

    added --invert and a warning notification

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
-// todo: polish and publish
+// todo: turn into a proper tui app
 
+use std::ops::Deref;
+use std::time::Duration;
 use clap::Parser;
 use tokio_i3ipc::I3;
 use anyhow::Result;
@@ -16,11 +18,14 @@ use tokio_i3ipc::msg::Msg;
 struct Cli {
     /// How long to lock the workspace for (ex: "1h 15min 3s")
     duration: humantime::Duration,
-    /// Workspace or workspaces to lock on, uses the curent one if not set
+    /// Workspace or workspaces to lock on, uses the current one if not set
     workspaces: Vec<String>,
     /// Start after a delay.
     #[arg(short,long)]
     delay: Option<humantime::Duration>,
+    /// Invert the selection aka allow every workspace that's not in the list.
+    #[arg(short,long)]
+    invert: bool,
 }
 
 #[tokio::main(flavor = "current_thread")]
@@ -40,13 +45,27 @@ async fn main() -> Result<()> {
     // delay
     if let Some(delay) = cli.delay {
         println!("Starting after {delay}");
-        sleep(delay.into()).await;
+        if delay.as_secs() > 15 {
+            sleep(
+                *delay.deref() - Duration::from_secs(10)).await;
+
+            Notification::new()
+                .summary("Workspaces locking in 10 seconds!")
+                .auto_icon()
+                .timeout(10_000)
+                .urgency(Urgency::Critical)
+                .show_async().await?;
+
+            sleep(Duration::from_secs(10)).await;
+        } else {
+            sleep(delay.into()).await;
+        }
     }
 
     // we check if the current workspace is allowed
     {
         let current_ws = get_focused_workspace(&mut i3).await?;
-        change_workspace_if_disallowed(current_ws.as_deref(), &mut i3, cli.workspaces.as_slice()).await?;
+        change_workspace_if_disallowed(current_ws.as_deref(), &mut i3, cli.workspaces.as_slice(), cli.invert).await?;
     }
 
     println!("Started!");
@@ -66,7 +85,7 @@ async fn main() -> Result<()> {
             event = listener.next() => {
                 let Event::Workspace(event) = event.expect("i3-ipc connection was closed!")? else {continue};
                 let name = event.current.and_then(|node|node.name);
-                change_workspace_if_disallowed(name.as_deref(), &mut i3, cli.workspaces.as_slice()).await?;
+                change_workspace_if_disallowed(name.as_deref(), &mut i3, cli.workspaces.as_slice(), cli.invert).await?;
             }
         }
 
@@ -88,13 +107,13 @@ async fn get_focused_workspace(i3: &mut I3) -> Result<Option<String>> {
     )
 }
 
-async fn change_workspace_if_disallowed(current_workspace: Option<&str>, i3: &mut I3, allowed_workspaces: &[String]) -> Result<()> {
-    if !allowed_workspaces.iter().any(|allowed| Some(allowed.as_str()) == current_workspace) {
+async fn change_workspace_if_disallowed(current_workspace: Option<&str>, i3: &mut I3, allowed_workspaces: &[String], invert: bool) -> Result<()> {
+    if invert ^ !allowed_workspaces.iter().any(|allowed| Some(allowed.as_str()) == current_workspace) {
         i3.send_msg_body(Msg::RunCommand, format!("workspace {}", allowed_workspaces[0]) ).await?;
         Notification::new()
             .summary("Workspace locked!")
             .auto_icon()
-            .timeout(2000)
+            .timeout(2_000)
             .urgency(Urgency::Critical)
             .show_async().await?;
     }
